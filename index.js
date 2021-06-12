24deb86200264c1aacde41a7055962ddf9732478
FMLjs <mrcofest@gmail.com>
2021-06-12 12:53:18 +0300

    first commit

--- index.js
+++ index.js
@@ -8,21 +8,9 @@ var cors = require('cors');
 
 app.use(cors())
 
-
-const getToken = async () => {
-
-    const result = await fetch('https://accounts.spotify.com/api/token', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/x-www-form-urlencoded',
-            'Authorization': 'Basic ' + Buffer.from(process.env.clientId + ':' + process.env.clientSecret).toString('base64')
-        },
-        body: 'grant_type=client_credentials'
-    });
-
-    const data = await result.json();
-    return data.access_token;
-}
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
 
 let mail = nodemailer.createTransport({
     host: 'smtp.gmail.com',
@@ -56,40 +44,4 @@ app.get('/send/:email', (req, res) => {
     });
 })
 
-// app.get('/token', async (req, res) => {
-//     const token = await (getToken());
-//     res.send(token);
-// })
-
-app.get('/song/:track', async (req, res) => {
-    const token = await (getToken());
-    const songs = await getTrack(token, req.params.track);
-    res.send(songs);
-})
-
-app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
-})
-
-const getTrack = async (token, track) => {
-
-    const result = await fetch(`https://api.spotify.com/v1/search?q=track:${track}&type=track&limit=8`, {
-        method: 'GET',
-        headers: { 'Authorization': 'Bearer ' + token }
-    });
-
-    const data = await result.json();
-    let values = [];
-    data.tracks.items.forEach(element => {
-        const a = {
-            name: element.artists[0].name,
-            trackName: element.name,
-            imgUrl: element.album.images[2].url,
-            uri: element.uri,
-            preview_url: element.preview_url
-        }
-        values.push(a);
-    });
-    return values;
-}
 
